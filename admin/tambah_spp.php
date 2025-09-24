91e5b70574b1b04a8eab8361380210aefd14d04b
DanuWN <ahmaddanuilhamdin77@gmail.com>
2025-09-25 05:57:55 +0800

    Fix reload tambah data

--- admin/tambah_spp.php
+++ admin/tambah_spp.php
@@ -7,74 +7,72 @@ if(isset($_POST['btnSimpan'])) {
     $nominal         = $_POST['nominal'];
 
     $pesanError = array();
-    if  (trim($id_spp)=="") {
-        $pesanError[] = "Data <b>ID SPP </b> tidak boleh kosong !";
-
+    if (trim($id_spp)=="") {
+        $pesanError[] = "Data <b>ID SPP</b> tidak boleh kosong!";
     }
-     if  (trim($tahun)=="") {
-        $pesanError[] = "Data <b>Tahun </b> tidak boleh kosong !";
-        
+    if (trim($tahun)=="") {
+        $pesanError[] = "Data <b>Tahun</b> tidak boleh kosong!";
     }
-     if  (trim($nominal)=="") {
-        $pesanError[] = "Data <b>Nominal </b> tidak boleh kosong !";
-        
+    if (trim($nominal)=="") {
+        $pesanError[] = "Data <b>Nominal</b> tidak boleh kosong!";
+    }
+    
+    // Check for duplicate ID SPP
+    if (trim($id_spp) != "") {
+        $checkId = mysqli_query($koneksi, "SELECT id_spp FROM spp WHERE id_spp = '$id_spp'");
+        if (mysqli_num_rows($checkId) > 0) {
+            $pesanError[] = "Data <b>ID SPP</b> sudah ada, gunakan ID lain!";
+        }
     }
 
-
-    if (count($pesanError)>=1 ){
-        $noPesan=0;
-        foreach ($pesanError as $indeks=>$pesan_tampil){
+    if (count($pesanError) >= 1) {
+        $noPesan = 0;
+        foreach ($pesanError as $indeks => $pesan_tampil) {
             $noPesan++;
-              echo  "&nbsp; $noPesan. $pesan_tampil<br>";
+            echo "&nbsp; $noPesan. $pesan_tampil<br>";
         }
         echo "<br>";
-
     }
     else {
         $sql = mysqli_query($koneksi, "INSERT INTO spp (id_spp, tahun, nominal)
-        VALUES('$id_spp','$tahun','$nominal')");
-        if($sql){
-        echo " <center> <b> <font color = 'red' size ='4'> <p> Data Berhasil Disimpan </p>
-        </center> </b> </font> </br>
-        <meta http-equiv='refresh' content='2'; url=?open=data_spp'>";
-    }
-        else{
-            echo "<center> <b> <font color = 'red' size = '4'> <p> Data Gagal Disimpan!
-        <meta http-equiv='refresh' content='2; url=?open=data_spp'>";
-
-    }
+            VALUES('$id_spp','$tahun','$nominal')");
+        if($sql) {
+            echo "<center><b><font color='green' size='4'><p>Data Berhasil Disimpan!</p></font></b></center><br>";
+            echo "<meta http-equiv='refresh' content='2; url=?open=data_spp'>";
+        }
+        else {
+            echo "<center><b><font color='red' size='4'><p>Data Gagal Disimpan!</p></font></b></center><br>";
+            echo "<meta http-equiv='refresh' content='2; url=?open=data_spp'>";
+        }
         exit;
+    }
 }
-}
-
 ?>
-<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" >
+
+<form action="" method="post">
     <table cellspacing="1" cellpadding="3">
         <tr>
-            <td bgcolor="CCCCCC"><strong>TAMBAH DATA SPP </strong></td>
-</tr>
-
-<tr>
-    <td>ID SPP </td>
-    <td>:</td>
-    <td><input name="id_spp" type="text" size="10" maxlength="4"  /></td>
-</tr>
-<tr>
-    <td>Tahun </td>
-    <td>:</td>
-     <td><input name="tahun" type="year" size="4" maxlength="4"  /></td>
-   </tr>
-
-   <tr>
-    <td>Nominal </td>
-    <td>:</td>
-     <td><input name="nominal" type="number" size="10" maxlength="10"  /></td>
-   </tr>
-   <tr>
-    <td>&nbsp;</td>
-     <td>&nbsp;</td>
-     <td><input name="btnSimpan" type="submit" value="Simpan" /></td>
-</tr>
-</table>
+            <td bgcolor="#CCCCCC"><strong>TAMBAH DATA SPP</strong></td>
+        </tr>
+        <tr>
+            <td>ID SPP</td>
+            <td>:</td>
+            <td><input name="id_spp" type="text" size="10" maxlength="10" /></td>
+        </tr>
+        <tr>
+            <td>Tahun</td>
+            <td>:</td>
+            <td><input name="tahun" type="number" size="4" maxlength="4" min="2020" max="2030" /></td>
+        </tr>
+        <tr>
+            <td>Nominal</td>
+            <td>:</td>
+            <td><input name="nominal" type="number" size="15" maxlength="15" /></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td><input name="btnSimpan" type="submit" value="Simpan" /></td>
+        </tr>
+    </table>
 </form>
-
